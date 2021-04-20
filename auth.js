92b74784d91e180dbe064952e15ca23c90a231b7
Anirban Das <anirbandas0206@gmail.com>
2021-04-20 21:12:15 +0530

    Client side authentication complete

--- auth.js
+++ auth.js
@@ -2,7 +2,11 @@ console.log("Auth Server..");
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const path = require('path');
+app.use(express.json({limit: '1mb'}));
+app.use(express.static(path.join(__dirname, 'views')));
 const jwt = require('jsonwebtoken');
+const mysql = require('mysql');
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -11,18 +15,14 @@ const transporter = nodemailer.createTransport({
        pass: process.env.EMAIL_PASS
     }
 });
-const mysql = require('mysql');
 
-app.use(express.json());
-
-//Create Connection
+////////////Database Connection ///////////////////////////////
 const db = mysql.createConnection({
     host: 'localhost',
     user:'root',
     password: '',
     database: 'proj_db'
 });
-
 //connect
 db.connect((err)=> {
     if(err)
@@ -31,12 +31,11 @@ db.connect((err)=> {
     }
     else console.log("MySql Connected");
 });
+////////////////////////////////////////////////////////////////////
 
-let listOfUser = new Map();
-listOfUser.set('hello','world');
-let otp = new Map();
 
-let refreshTokens = [];
+
+////////////AUTHENTICATION///////////////////////////////////////////
 
 //create new token using refresh token
 app.post('/token',(req,res) => {
@@ -62,47 +61,47 @@ app.post('/login',(req,res) =>{
     //Authenticate users
     const username = req.body.username;
     const password = req.body.password;
-    //console.log("result: "+username);
-    
+
     let sqluid = "SELECT COUNT(*) as `count` FROM `voters` WHERE `userid` = ? ";
     db.query(sqluid, [username], (err, result)=>{
         if(err)
         {
-            throw err;
+            res.sendStatus(500);
+            return;
         }
         else {
-            console.log("result : "+result[0][`count`]);
+            //console.log("result : "+result[0][`count`]);
             if(result[0][`count`] == 0){
 
-                console.log("Not Valid!");
-                return res.status(403).send({message:'Username is not valid!'});
+                res.status(403).send({message:'Username is not valid!',check: '0'});
+                return;
             }
-            else console.log("Valid User");
+            else{
+                 
+                let sqlpass = "SELECT password as `pw` FROM `voters` WHERE `userid` = ? ";
+                db.query(sqlpass, [username], (err, result)=>{
+                    if(err)
+                    {
+                        res.sendStatus(500);
+                    }
+                    else {
+                        if(result[0][`pw`] !== password){
+                            res.status(403).send({message:'Wrong Credentials!',check: '0'});
+                            return;
+                        }
+                        else{
+                            //Create new tokens on login
+                            const user = {name: username};  
+                            const accessToken = generateAccessToken(user);
+                            const refreshToken = jwt.sign(user,process.env.REFRESH_TOKEN_SECRET);
+                            res.json({username: username,accessToken: accessToken, refreshToken: refreshToken,message : 'login successful',redirect: '/dashboard',check: '1'});
+                        }
+                    }
+                })
+
+            }
         }
     });
-
-    let sqlpass = "SELECT password as `pw` FROM `voters` WHERE `userid` = ? ";
-    db.query(sqlpass, [username], (err, result)=>{
-        if(err)
-        {
-            throw err;
-        }
-        else {
-            console.log("result : "+result[0][`pw`]);
-            if(result[0][`pw`] !== password){
-
-                console.log("Not Valid!");
-                return res.status(403).send({message:'Wrong Credentials!'});
-            }
-            else console.log("Valid User");
-        }
-    });
-
-    //Create new tokens on login
-    const user = {name: username};  
-    const accessToken = generateAccessToken(user);
-    const refreshToken = jwt.sign(user,process.env.REFRESH_TOKEN_SECRET);
-    res.json({accessToken: accessToken, refreshToken: refreshToken,message : 'login successful'});
 })
 
 app.post('/signup',(req,res) =>{
@@ -110,50 +109,78 @@ app.post('/signup',(req,res) =>{
     const username = req.body.username;
     const password = req.body.password;
     const emailId = req.body.emailId;
-    const otp = sendOtp(emailId);
-    console.log("OTP : "+otp);
-    
-    //sql username password emailid Otp - temporary db
-    let sqlcount = "SELECT COUNT(*) as `count` FROM `temp` WHERE `userid` = ? AND `emailid` = ?";
-    db.query(sqlcount, [username, emailId], (err, result)=>{
+    let sqluid = "SELECT COUNT(*) as `count` FROM `voters` WHERE `userid` = ? ";
+    db.query(sqluid, [username], (err, result)=>{
+
         if(err)
         {
-            throw err;
+            res.sendStatus(500);
+            return;
         }
-        else 
+        else if(result[0][`count`] !== 0){
+
+            res.json({check: '0',message: 'This username already exist'});
+            return;
+        }
+        else
         {
-            console.log("result : "+result[0][`count`]);
-            if(result[0][`count`] == 0){
-                let sql = "INSERT INTO temp VALUES(?, ?, ?, ?)";
-                db.query(sql, [username, password, emailId, otp], (err, result)=>{
-                    if(err)
-                    {
-                        throw err;
-                    }
-                    else {
-                        console.log("OTP Inserted!"+result);
-                    }
-                });
-                //return res.status(403).send({message:'Username is not valid!'});
-            }
-            else 
-            {
-                let sql = "UPDATE `temp` SET `otp` = ? WHERE `userid` = ? AND `emailid` = ?";
-                db.query(sql, [otp, username, emailId], (err, result)=>{
-                    if(err)
-                    {
-                        throw err;
-                    }
-                    else {
-                        console.log("OTP Updated!"+result);
-                    }
-                });
-                //return res.status(403).send({message:'Username is not valid!'});
-            }
+            let sqlemail = "SELECT COUNT(*) as `count` FROM `voters` WHERE `emailid` = ? ";
+            
+            db.query(sqlemail,[emailId],(err,result) =>{
+
+                if(err)
+                res.sendStatus(500);
+                else if(result[0][`count`]!== 0){
+                    res.json({check: '0',message: 'This email already exist'});
+                    return;
+                }
+                else{
+                   
+                    const otp = sendOtp(emailId);
+                    let sqlcount = "SELECT COUNT(*) as `count` FROM `temp` WHERE `userid` = ? AND `emailid` = ?";
+                    
+                    db.query(sqlcount, [username, emailId], (err, result)=>{
+                           
+                        if(err)
+                            res.sendStatus(500);
+                        else 
+                        {
+                            console.log("result : "+result[0][`count`]);
+                            if(result[0][`count`] == 0){
+                                let sql = "INSERT INTO temp VALUES(?, ?, ?, ?)";
+                                db.query(sql, [username, password, emailId, otp], (err, result)=>{
+                                    if(err)
+                                    {
+                                        throw err;
+                                    }
+                                    else {
+                                        console.log("OTP Inserted!"+result);
+                                    }
+                                });
+                            }
+                            else 
+                            {
+                                let sql = "UPDATE `temp` SET `otp` = ? WHERE `userid` = ? AND `emailid` = ?";
+                                db.query(sql, [otp, username, emailId], (err, result)=>{
+                                    if(err)
+                                    {
+                                        throw err;
+                                    }
+                                    else {
+                                        console.log("OTP Updated!"+result);
+                                    }
+                                });
+                            }
+                        }
+                        
+
+                    })
+                    res.status(200).send({check:'1',message:'OTP send successfully'})
+
+                }
+            })
         }
-    });
-    
-    res.status(200).send({message:'OTP send successfully'});
+    })
 
 })
 
@@ -190,13 +217,13 @@ app.post('/checkotp',(req,res)=>{
                         }
                      });
 
-                    res.status(200).send({message: "Account created & verified"});
+                    res.status(200).send({check: '1',message: "Account created & verified"});
                 }
                 else {
                     
                     console.log("Wrong OTP!");
-                    res.status(403).send({message: "Wrong OTP!"});
-            }
+                    res.status(403).send({check: '0',message: "Wrong OTP!"});
+                     }
         }
     });
 
@@ -243,5 +270,36 @@ function authenticateToken(req,res,next){
    })
 }
 */
+
+///////////////////////////////////////////////////////////////////////////////////
+
+
+
+/////////////////Routing///////////////////////////////////////////////////////////
+
+
+
+app.get('/',(req,res) => {
+    
+    res.sendFile('./views/html/index.html',{root: __dirname});
+});
+app.get('/dashboard',(req,res) => {
+    
+    res.sendFile('./views/html/dashboard.html',{root: __dirname});
+});
+app.get('/home',(req,res) => {
+    
+    res.sendFile('./views/html/dashboard.html',{root: __dirname});
+});
+app.get('/profile',(req,res) => {
+    
+    res.sendFile('./views/html/profile.html',{root: __dirname});
+});
+app.get('/about',(req,res) => {
+    
+    res.sendFile('./views/html/about.html',{root: __dirname});
+});
+
+/////////////////////////////////////////////////////////////////////////////////
 
 app.listen(4000);
