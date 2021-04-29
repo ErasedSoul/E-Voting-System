8ad9677535c0a189683de80489a6328c1fcd6649
sayanti3739 <40183509+sayanti3739@users.noreply.github.com>
2021-04-30 00:19:33 +0530

    Add files via upload

--- views/js/ballotInviteOngoing.js
+++ views/js/ballotInviteOngoing.js
@@ -3,7 +3,8 @@ async function showBallot(){
     const url = "http://localhost:4000/invite/ongoing";
  
     let res  = await fetch(url,{
-     method: 'POST', 
+     method: 'POST',
+     withCredentials: true,credentials: 'include', 
      headers: {
        'Content-Type': 'application/json'
      },
@@ -12,8 +13,8 @@ async function showBallot(){
      })
      })
  
-    let data = await res.json();
-    ballots = data.ballots;
+    let ballots = await res.json();
+    //ballots = data.ballots;
  
     console.log(ballots.length);
  
@@ -43,10 +44,10 @@ async function showBallot(){
              }); 
        });
           */
-         card.innerHTML+= "<p>" + ballots[i].bname + "<br>" +
-         "ballot id :"+ballots[i].bid+"<br>"+
-         "starting from:" + ballots[i].startTime + "<br>" +
-         "ending at:" + ballots[i].endTime + "</p>";
+       card.innerHTML+= "<p>" + ballots[i].bname + "<br>" +
+       "ballot id :"+ballots[i].ballotid+"<br>"+
+       "starting from:" + ballots[i].startdate + "<br>" +
+       "ending at:" + ballots[i].enddate + "</p>";
          
          ballotList.appendChild(card);
     }
